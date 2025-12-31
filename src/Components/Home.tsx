be2f5fb0b22be73a3f124942070c7d5521df9e55
NarsimhaReddyKK <narsimhareddy.kandi@wisseninfotech.com>
2025-12-31 17:28:21 +0530

    Added Total Card

--- src/Components/Home.tsx
+++ src/Components/Home.tsx
@@ -6,10 +6,11 @@ export const Home = () => {
   return (
     <div>
         <Header/>
-        <div className="card__container"> 
+        <div className="card__container">
             <Card count={0} name="Open"></Card>
             <Card count={0} name="InProgress"></Card>
             <Card count={0} name="Closed"></Card>
+            <Card count={0} name="Total"></Card>
         </div>
     </div>
   )

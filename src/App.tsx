6a89e33808b1b50e9819f487154bd057b951c0a5
NarsimhaReddyKK <narsimhareddy.kandi@wisseninfotech.com>
2025-12-31 15:48:35 +0530

    Added Header and Routed paths

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css'
 import { Ticket_Form } from './Components/Ticket_Form'
+import { Home } from './Components/Home'
 
 function App() {
 
   return (
-    <>
-      <Ticket_Form/>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/form' element={<Ticket_Form/>}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ src/Components/header.tsx
@@ -0,0 +1,15 @@
+import "./styles/header.css"
+import { Link } from "react-router-dom";
+
+export const Header = () => {
+  return (
+    <div className="header_container">
+        <ul className="header">
+            <li className="header__list"><Link to="/">Home</Link></li>
+            <li className="header__list"><Link to="/">Admin</Link></li>
+            <li className="header__list"><Link to="/form">Form</Link></li>
+            <li className="header__list"><Link to="/">About</Link></li>
+        </ul>
+    </div>
+  )
+}

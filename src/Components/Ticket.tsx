22f2f08baae31b33bfc0234b7e09c04d7d644adf
NarsimhaReddyKK <narsimhareddy.kandi@wisseninfotech.com>
2026-01-05 16:10:40 +0530

    Added color pallete to ticket state

--- src/Components/Ticket.tsx
+++ src/Components/Ticket.tsx
@@ -1,27 +1,41 @@
 import "./styles/Ticket.css"
 
-type TicketProp ={
-    id:number,
-    title:string,
-    state: string,
-    desc: string,
-    timestamp: string
+type TicketProp = {
+  id: number,
+  title: string,
+  state: string,
+  desc: string,
+  timestamp: string
 }
 
-export const Ticket = ({id, title, state, desc, timestamp}:TicketProp) => {
+export const Ticket = ({ id, title, state, desc, timestamp }: TicketProp) => {
   return (
     <div className="ticket">
-        <div className="ticket__details">
-          <div className="ticket__header">
-            <h2 className="ticket__id">#{id} </h2>
-            <span className="ticket__timestamp">{timestamp}</span>
-          </div>
-            <h1 className="ticket__title"><span className="ticket__title-span">{title}</span></h1>
-            <h2 className="ticket__id">{desc}</h2>
+      <div className="ticket__details">
+        <div className="ticket__header">
+          <h2 className="ticket__id">#{id} </h2>
+          <span className="ticket__timestamp">{timestamp}</span>
         </div>
-        <h2 className="ticket__status">
-            Status: <span className="ticket__state">{state}</span>
-        </h2>
+        <h1 className="ticket__title"><span className="ticket__title-span">{title}</span></h1>
+        <h2 className="ticket__id">{desc}</h2>
+      </div>
+      <h2 className="ticket__status">
+        Status: <span
+          className={
+            "ticket__state" +
+            (state === "OPEN"
+              ? " opent"
+              : state === "IN_PROGRESS"
+                ? " inprogresst"
+                : state === "RESOLVED"
+                  ? " resolvedt"
+                  : "")
+          }
+        >
+          {state}
+        </span>
+
+      </h2>
     </div>
   )
 }
